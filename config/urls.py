55af3200c595638546785f964c2a8b992a8ff3d3
Ragul rao07 <ragulrao2004@gmail.com>
2026-09-13 15:03:44 +0530

    Update urls configuration and route handling

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.http import HttpResponse
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    # Automatically forward root to the admin dashboard
+    path('', RedirectView.as_view(url='admin/', permanent=False)),
+    
+    # Silence favicon 404s
+    path('favicon.ico', lambda request: HttpResponse(status=204)),
+    
+    # Admin interface
     path('admin/', admin.site.urls),
 ]
